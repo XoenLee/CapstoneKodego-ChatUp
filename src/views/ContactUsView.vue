d1e4fe3e0795ca08fa62d79f946f93a87fb56b1f
Kenneth Lagos <104002563+Kikyukew123@users.noreply.github.com>
2022-08-10 22:34:13 +0800

    Update ContactUsView.vue
    contact us form validation

--- src/views/ContactUsView.vue
+++ src/views/ContactUsView.vue
@@ -1,36 +1,37 @@
 <template>
 <div class="contact">
     <div class="container my-5">
-        <div class="row">
-            <div class="contact col-lg-6 my-3 gx-5 py-5">
-                <img class="container__contact-img text-center mb-5" src="../assets/contact-image.png" alt="head">
-            </div>
+            <div class="row">
+                <div class="contact col-lg-6 my-3 gx-5 py-5">
+                    <img class="container__contact-img text-center mb-5" src="../assets/contact-image.png" alt="head">
+                </div>
+                <div class="container__form-section col-lg-6 my-5 gx-5 py-5">
+                    <div class="form" >
+                    <form  @submit.prevent="handleSubmit">
 
-            <div class="container__form-section col-lg-6 my-5 gx-5 py-5">
-                <div class="form">
-                    <div>
-                        <h2 class="container__sign-heading text-center">CONTACT US!</h2>
-                        <h5 class="container__sign-subHeading text-center mb-5">Any question or suggestion? Shoot us a message.</h5>
-                    </div>
-                    <div>
-                        <label for="name">Name</label>
-                        <input type="text"  class="form-control border-bottom" placeholder="John Doe">
-                    </div>
-                
-                <br>
-                    <div>
-                        <label for="email">Email</label>
-                        <input type="email"  class="form-control" placeholder="johndoe@gmail.com">
-                    </div>
-                <br>
-                    <div class="mb-3">
-                        <label for="Textarea" class="form-label">Message</label>
-                        <textarea class="form-control" id="FormControlTextarea" rows="5" placeholder="Write your message here..."></textarea>
-                    </div>
-                    <div class="container__button">
-                    <button class="container__btn2 mx-0 mb-3 w-100">SEND MESSAGE</button>
-                    </div>
-
+                        <div>
+                            <h2 class="container__sign-heading text-center">CONTACT US!</h2>
+                            <h5 class="container__sign-subHeading text-center mb-5">Any question or suggestion? Shoot us a message.</h5>
+                        </div>
+                        <div>
+                            <label for="name">Name</label>
+                            <input type="text"  class="form-control border-bottom" placeholder="John Doe" v-model="name">
+                        </div>
+                    
+                    <br>
+                        <div>
+                            <label for="email">Email</label>
+                            <input type="email"  class="form-control" placeholder="johndoe@gmail.com"  v-model="email">
+                        </div>
+                    <br>
+                        <div class="mb-3">
+                            <label for="Textarea" class="form-label">Message</label>
+                            <textarea class="form-control" id="FormControlTextarea" rows="5" placeholder="Write your message here..."  v-model="message"></textarea>
+                        </div>
+                        <div class="container__button"  >
+                        <button class="container__btn2 mx-0 mb-3 w-100" type="submit">SEND MESSAGE</button>
+                        </div>
+                </form>
                 </div>
                 <!-- End of form -->
                 <div class="m-3 text-center">
@@ -38,18 +39,39 @@
                     <p class="container__email-us border-bottom">chatup-message@gmail.com</p>
                 </div>
                 <router-link to="/">
-                    <p class="text-center mt-5"> Go Back to Homepage </p>
+                    <p class="text-center mt-5" > Go Back to Homepage </p>
                 </router-link>
                 
             </div>
+            
+
+
         </div>
     </div>
 </div>
 </template>
 
 <script>
+import {projectFirestore} from '../firebase/config'
     export default{
-        name:'ContactUsView'
+ data() {
+    return {
+        name: '',
+        email: '',
+        message: ''
+    }
+},
+ methods: {
+  handleSubmit() {
+    let userMessage = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    }
+
+    projectFirestore.collection('userMessages').add(userMessage)
+        }
+    }
     }
 </script>
 
